7254b6628ab71400f4d4c4902ba1116c86471eed
TesterTesterson1004 <49700217+TesterTesterson1004@users.noreply.github.com>
2024-12-17 16:54:01 -0800

    Create setup2.ps1

---
+++ Allfiles/labs/05/setup2.ps1
@@ -0,0 +1,208 @@
+Clear-Host
+write-host "Starting script at $(Get-Date)"
+
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-Module -Name Az.Synapse -Force
+Install-Module -Name AzExpression
+
+# Handle cases where the user has multiple subscriptions
+$subs = Get-AzSubscription | Select-Object
+if($subs.GetType().IsArray -and $subs.length -gt 1){
+        Write-Host "You have multiple Azure subscriptions - please select the one you want to use:"
+        for($i = 0; $i -lt $subs.length; $i++)
+        {
+                Write-Host "[$($i)]: $($subs[$i].Name) (ID = $($subs[$i].Id))"
+        }
+        $selectedIndex = -1
+        $selectedValidIndex = 0
+        while ($selectedValidIndex -ne 1)
+        {
+                $enteredValue = Read-Host("Enter 0 to $($subs.Length - 1)")
+                if (-not ([string]::IsNullOrEmpty($enteredValue)))
+                {
+                    if ([int]$enteredValue -in (0..$($subs.Length - 1)))
+                    {
+                        $selectedIndex = [int]$enteredValue
+                        $selectedValidIndex = 1
+                    }
+                    else
+                    {
+                        Write-Output "Please enter a valid subscription number."
+                    }
+                }
+                else
+                {
+                    Write-Output "Please enter a valid subscription number."
+                }
+        }
+        $selectedSub = $subs[$selectedIndex].Id
+        Select-AzSubscription -SubscriptionId $selectedSub
+        az account set --subscription $selectedSub
+}
+
+# Prompt user for a password for the SQL Database
+$sqlUser = "SQLUser"
+write-host ""
+$sqlPassword = ""
+$complexPassword = 0
+
+while ($complexPassword -ne 1)
+{
+    $SqlPassword = Read-Host "Enter a password to use for the $sqlUser login.
+    `The password must meet complexity requirements:
+    ` - Minimum 8 characters. 
+    ` - At least one upper case English letter [A-Z]
+    ` - At least one lower case English letter [a-z]
+    ` - At least one digit [0-9]
+    ` - At least one special character (!,@,#,%,^,&,$)
+    ` "
+
+    if(($SqlPassword -cmatch '[a-z]') -and ($SqlPassword -cmatch '[A-Z]') -and ($SqlPassword -match '\d') -and ($SqlPassword.length -ge 8) -and ($SqlPassword -match '!|@|#|%|\^|&|\$'))
+    {
+        $complexPassword = 1
+	    Write-Output "Password $SqlPassword accepted. Make sure you remember this!"
+    }
+    else
+    {
+        Write-Output "$SqlPassword does not meet the complexity requirements."
+    }
+}
+
+# Register resource providers
+Write-Host "Registering resource providers...";
+$provider_list = "Microsoft.Synapse", "Microsoft.Sql", "Microsoft.Storage", "Microsoft.Compute"
+foreach ($provider in $provider_list){
+    $result = Register-AzResourceProvider -ProviderNamespace $provider
+    $status = $result.RegistrationState
+    Write-Host "$provider : $status"
+}
+
+function Get-UniqueString {
+    param (
+        [string[]]$Inputs  # Array of input strings
+    )
+
+    # Combine all input strings into one
+    $combinedString = ($Inputs -join '')
+
+    # Compute the SHA-256 hash
+    $sha256 = [System.Security.Cryptography.SHA256]::Create()
+    $hashBytes = $sha256.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($combinedString))
+
+    # Encode the hash to Base64
+    $base64Hash = [Convert]::ToBase64String($hashBytes)
+
+    # Truncate to the first 13 characters
+    return $base64Hash.Substring(0, 13)
+}
+
+$sub = az account show --query "id" --output tsv
+$sublabid = az tag list --resource-id "/subscriptions/$sub" --query "properties.tags.LabInstance" --output tsv
+
+# Use a specific string as input (no user input required)
+[string]$inputString = $sublabid
+
+# Generate a deterministic unique suffix
+[string]$suffix = New-AzUniqueString -InputStrings $sublabid
+
+# Display the generated suffix
+Write-Host "Your deterministic unique suffix for Azure resources is" $suffix
+
+# Use the unique suffix to create a resource group name
+$resourceGroupName = "dp203"
+
+# Display the full resource group name
+Write-Host "Your resource group name is" $resourceGroupName
+
+# Choose a random region
+Write-Host "Finding an available region. This may take several minutes...";
+$delay = 0, 30, 60, 90, 120 | Get-Random
+Start-Sleep -Seconds $delay # random delay to stagger requests from multi-student classes
+$preferred_list = "australiaeast","centralus","southcentralus","eastus2","northeurope","southeastasia","uksouth","westeurope","westus","westus2"
+$locations = Get-AzLocation | Where-Object {
+    $_.Providers -contains "Microsoft.Synapse" -and
+    $_.Providers -contains "Microsoft.Sql" -and
+    $_.Providers -contains "Microsoft.Storage" -and
+    $_.Providers -contains "Microsoft.Compute" -and
+    $_.Location -in $preferred_list
+}
+$max_index = $locations.Count - 1
+$rand = (0..$max_index) | Get-Random
+$Region = $locations.Get($rand).Location
+
+# Test for subscription Azure SQL capacity constraints in randomly selected regions
+# (for some subsription types, quotas are adjusted dynamically based on capacity)
+ $success = 0
+ $tried_list = New-Object Collections.Generic.List[string]
+
+ while ($success -ne 1){
+    write-host "Trying $Region"
+    $capability = Get-AzSqlCapability -LocationName $Region
+    if($capability.Status -eq "Available")
+    {
+        $success = 1
+        write-host "Using $Region"
+    }
+    else
+    {
+        $success = 0
+        $tried_list.Add($Region)
+        $locations = $locations | Where-Object {$_.Location -notin $tried_list}
+        if ($locations.Count -ne 1)
+        {
+            $rand = (0..$($locations.Count - 1)) | Get-Random
+            $Region = $locations.Get($rand).Location
+        }
+        else {
+            Write-Host "Couldn't find an available region for deployment."
+            Write-Host "Sorry! Try again later."
+            Exit
+        }
+    }
+}
+Write-Host "Creating $resourceGroupName resource group in $Region ..."
+New-AzResourceGroup -Name $resourceGroupName -Location $Region | Out-Null
+
+# Create Synapse workspace
+$lowerCaseAccountName1 = "synapse$suffix"
+$synapseWorkspace = $lowerCaseAccountName1.ToLower()
+$lowerCaseAccountName2 = "datalake$suffix"
+$dataLakeAccountName = $lowerCaseAccountName2.ToLower()
+$lowerCaseAccountName3 = "spark$suffix"
+$sparkPool = $lowerCaseAccountName3.ToLower()
+
+write-host "Creating $synapseWorkspace Synapse Analytics workspace in $resourceGroupName resource group..."
+write-host "(This may take some time!)"
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName `
+  -TemplateFile "setup.json" `
+  -Mode Complete `
+  -workspaceName $synapseWorkspace `
+  -dataLakeAccountName $dataLakeAccountName `
+  -sparkPoolName $sparkPool `
+  -sqlUser $sqlUser `
+  -sqlPassword $sqlPassword `
+  -uniqueSuffix $suffix `
+  -Force
+
+# Make the current user and the Synapse service principal owners of the data lake blob store
+write-host "Granting permissions on the $dataLakeAccountName storage account..."
+write-host "(you can ignore any warnings!)"
+$subscriptionId = (Get-AzContext).Subscription.Id
+$userName = ((az ad signed-in-user show) | ConvertFrom-JSON).UserPrincipalName
+$id = (Get-AzADServicePrincipal -DisplayName $synapseWorkspace).id
+New-AzRoleAssignment -Objectid $id -RoleDefinitionName "Storage Blob Data Owner" -Scope "/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.Storage/storageAccounts/$dataLakeAccountName" -ErrorAction SilentlyContinue;
+New-AzRoleAssignment -SignInName $userName -RoleDefinitionName "Storage Blob Data Owner" -Scope "/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.Storage/storageAccounts/$dataLakeAccountName" -ErrorAction SilentlyContinue;
+
+# Upload files
+write-host "Loading data..."
+$storageAccount = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $dataLakeAccountName
+$storageContext = $storageAccount.Context
+Get-ChildItem "./data/*.csv" -File | Foreach-Object {
+    write-host ""
+    $file = $_.Name
+    Write-Host $file
+    $blobPath = "sales/orders/$file"
+    Set-AzStorageBlobContent -File $_.FullName -Container "files" -Blob $blobPath -Context $storageContext
+}
+
+write-host "Script completed at $(Get-Date)"
